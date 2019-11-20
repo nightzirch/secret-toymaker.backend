d85578b930a0fb2d69609bb6d6a3ce9c615859f3
Brendan Golden <23silver@gmail.com>
2019-11-20 03:14:27 +0000

    cleaned up stages, still returns int eh same format

--- functions/https/stage.js
+++ functions/https/stage.js
@@ -2,55 +2,22 @@ const functions = require("firebase-functions");
 require("firebase/firestore");
 
 const db = require("../config/db");
-const { STAGES } = require("../config/constants");
+const { STAGES , YEAR} = require("../config/constants");
 
-const moment = require("moment");
-moment.locale("nb");
+// this gets teh current stage info
+module.exports = functions.https.onCall(async(data, context) => {
+  let currentStage = { name: STAGES.INACTIVE, start: null, end: null }
 
-module.exports = functions.https.onCall((data, context) => {
-  return db
-    .collection("timebox")
-    .get()
-    .then(snapshot => {
-      const now = moment();
-      var stage = {};
+  let stages =  await db.collection('events').doc(YEAR).collection('timebox').get()
 
-      snapshot.forEach(doc => {
-        var t = doc.data();
+  if (stages.empty) {return currentStage}
 
-        Object.keys(STAGES).map(s => {
-          if (
-            t[STAGES[s]] &&
-            now.isBetween(
-              t[STAGES[s]].start.toDate(),
-              t[STAGES[s]].end.toDate(),
-              "day",
-              "[]"
-            )
-          ) {
-            stage = {
-              name: STAGES[s],
-              end: t[STAGES[s]].end.toDate().getTime()
-            };
-          }
-
-          if (!stage) {
-            stage = {
-              name: STAGES.INACTIVE,
-              end: null
-            };
-          }
-        });
-      });
-
-      return {
-        stage
-      };
-    })
-    .catch(err => {
-      console.log("Error when getting stage:", err);
-      return {
-        error: err
-      };
-    });
+  let now = new Date().toISOString()
+  stages.forEach(doc => {
+    let stage = doc.data()
+    if(stage.start < now && now < stage.end){
+      currentStage = { name: doc.id, start: new Date(stage.start).getTime(), end: new Date(stage.end).getTime() }
+    }
+  })
+  return currentStage
 });
