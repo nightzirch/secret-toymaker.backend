8f60be03d2b7883be8ee7e99a827936ed9bd4754
Brendan Golden <23silver@gmail.com>
2019-12-08 15:28:32 +0000

    spacing due to documentation

--- https/participate.js
+++ https/participate.js
@@ -22,58 +22,58 @@ const participate = functions.https.onCall(
    * @returns {Result}
    */
   async ({user, participate}, context) => {
-  let uuid = await getUUID(user)
-  if(uuid.error){return {error: "no API key set"}}
-  uuid = uuid.success
+    let uuid = await getUUID(user)
+    if (uuid.error) {return { error: "no API key set" }}
+    uuid = uuid.success
 
-  if(!participate){
-    // user wishes to undo their participation
+    if (!participate) {
+      // user wishes to undo their participation
 
-    let deleteDoc =  await db.collection('events').doc(EVENT).collection('participants').doc(uuid).delete().then(()=> {return true}).catch(() => {return false})
-    let counter = await db.collection('events').doc(EVENT).set({participants: admin.firestore.FieldValue.increment(-1)}, {merge: true}).then(()=> {return true}).catch(() => {return false});
-    if(deleteDoc && counter){
-      return {success: "Successfully removed"}
-    }else{
-      return {error: "Error removing participant"}
+      let deleteDoc = await db.collection('events').doc(EVENT).collection('participants').doc(uuid).delete().then(() => {return true}).catch(() => {return false})
+      let counter = await db.collection('events').doc(EVENT).set({ participants: admin.firestore.FieldValue.increment(-1) }, { merge: true }).then(() => {return true}).catch(() => {return false});
+      if (deleteDoc && counter) {
+        return { success: "Successfully removed" }
+      } else {
+        return { error: "Error removing participant" }
+      }
     }
-  }
 
     // check if already exists
     let currentValueRaw = await db.collection('events').doc(EVENT).collection('participants').doc(uuid).get()
-    if (currentValueRaw.exists) {return {success: "Already added"}}
+    if (currentValueRaw.exists) {return { success: "Already added" }}
 
-  let gameAccount = await getGw2Account(uuid)
+    let gameAccount = await getGw2Account(uuid)
 
-  // use uuid to set teh game accoutn for entry
-  let entry = {
-    participant: uuid,
-    entered: new Date().toISOString(),
+    // use uuid to set teh game accoutn for entry
+    let entry = {
+      participant: uuid,
+      entered: new Date().toISOString(),
 
-    // this marks if they have sent their own gift
-    sent_own: false,
+      // this marks if they have sent their own gift
+      sent_own: false,
 
-    // these manage the status of this persons gift
-    sent:false,
-    received: false,
-    reported: false,
+      // these manage the status of this persons gift
+      sent: false,
+      received: false,
+      reported: false,
 
-    // these manage who is gifting to them and who they are gifting to
-    giftee: null,
-    gifter: null,
+      // these manage who is gifting to them and who they are gifting to
+      giftee: null,
+      gifter: null,
 
-    // add this here, will save a call later
-    name:gameAccount.success.id,
-    // mark if the account is F2P
-    freeToPlay:gameAccount.success.freeToPlay
-  }
-  let entryResult = await db.collection('events').doc(EVENT).collection('participants').doc(uuid).set(entry).then(()=> {return true}).catch(() => {return false});
-  let counter = await db.collection('events').doc(EVENT).set({participants: admin.firestore.FieldValue.increment(1)}, {merge: true}).then(()=> {return true}).catch(() => {return false});
-  // check result and return to frontend
-  if(entryResult && counter){
-    return {success: "Successfully added"}
-  }else{
-    return {error: "Error entering participant"}
-  }
-})
+      // add this here, will save a call later
+      name: gameAccount.success.id,
+      // mark if the account is F2P
+      freeToPlay: gameAccount.success.freeToPlay
+    }
+    let entryResult = await db.collection('events').doc(EVENT).collection('participants').doc(uuid).set(entry).then(() => {return true}).catch(() => {return false});
+    let counter = await db.collection('events').doc(EVENT).set({ participants: admin.firestore.FieldValue.increment(1) }, { merge: true }).then(() => {return true}).catch(() => {return false});
+    // check result and return to frontend
+    if (entryResult && counter) {
+      return { success: "Successfully added" }
+    } else {
+      return { error: "Error entering participant" }
+    }
+  })
 
 module.exports = { participate }
