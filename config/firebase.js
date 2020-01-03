dc1fab7109b04a807031ba53d8e74544588d4d96
Unknown <Christian.Grimsgaard@knowit.no>
2020-01-03 14:42:57 +0100

    Removed firebase.auth for now

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,8 @@
 const admin = require('firebase-admin');
-require('firebase/auth');
 require('firebase/firestore');
 
 admin.initializeApp();
 
 const db = admin.firestore();
-const auth = admin.auth();
 
-module.exports = { db, auth };
+module.exports = { db };
