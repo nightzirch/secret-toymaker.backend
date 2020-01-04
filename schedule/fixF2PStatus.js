fb079283ad24a3b1a89475ff3810851bc6fc5d46
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2020-01-04 16:25:37 +0100

    Prettier. Sorry, Silver :D

--- schedule/fixF2PStatus.js
+++ schedule/fixF2PStatus.js
@@ -2,39 +2,53 @@ const CollectionTypes = require("../utils/types/CollectionTypes");
 
 const functions = require("firebase-functions");
 require("firebase/firestore");
-const {  getGw2Account } = require("../utils/utils");
+const { getGw2Account } = require("../utils/utils");
 const { EVENT } = require("../config/constants");
 const { db } = require("../config/firebase");
 const rp = require("request-promise-native");
 
-const fixF2PStatus = functions.pubsub.schedule("0 0 1 1 *").onRun(
-  async (context) => {
+const fixF2PStatus = functions.pubsub
+  .schedule("0 0 1 1 *")
+  .onRun(async context => {
     const eventDoc = db.collection(CollectionTypes.EVENTS).doc(EVENT);
 
     // get all users that have the f2p flag
-    let allParticipants = await eventDoc.collection(CollectionTypes.EVENTS__PARTICIPANTS).where("isFreeToPlay", "==", true).get()
+    let allParticipants = await eventDoc
+      .collection(CollectionTypes.EVENTS__PARTICIPANTS)
+      .where("isFreeToPlay", "==", true)
+      .get();
 
     // grab their api key and run it through again
-    let allParticipantsData = []
+    let allParticipantsData = [];
 
-    allParticipants.forEach(doc => {allParticipantsData.push(doc.data())})
+    allParticipants.forEach(doc => {
+      allParticipantsData.push(doc.data());
+    });
 
-    for(let i=0;i<allParticipantsData.length;i++){
+    for (let i = 0; i < allParticipantsData.length; i++) {
       // eslint-disable-next-line no-await-in-loop
-      let gameAccount = await getGw2Account(allParticipantsData[i].gameAccountUUID)
-      if(gameAccount.success){
-
+      let gameAccount = await getGw2Account(
+        allParticipantsData[i].gameAccountUUID
+      );
+      if (gameAccount.success) {
         // pass the key to the function
         // eslint-disable-next-line no-await-in-loop
-        await updateF2P (gameAccount.success.apiToken)
+        await updateF2P(gameAccount.success.apiToken);
       }
     }
-  }
-)
+  });
 
-async function updateF2P (apiToken){
-  let url = "https://api.guildwars2.com/v2/account?v=2019-11-18T00:00:00Z&access_token=" + apiToken;
-  let accountData = await rp({ url: url, resolveWithFullResponse: true }).then(response => {return { headers: response.headers, body: response.body };}).catch(error => {return { error: error };});
+async function updateF2P(apiToken) {
+  let url =
+    "https://api.guildwars2.com/v2/account?v=2019-11-18T00:00:00Z&access_token=" +
+    apiToken;
+  let accountData = await rp({ url: url, resolveWithFullResponse: true })
+    .then(response => {
+      return { headers: response.headers, body: response.body };
+    })
+    .catch(error => {
+      return { error: error };
+    });
 
   if (accountData.error) {
     // Somethign went wrong went wrong
@@ -52,23 +66,24 @@ async function updateF2P (apiToken){
   let result = JSON.parse(accountData.body);
 
   // figure pout if the person is F2P
-  let isFreeToPlay = result.access.indexOf("PlayForFree") !== -1 && result.access.length === 1;
+  let isFreeToPlay =
+    result.access.indexOf("PlayForFree") !== -1 && result.access.length === 1;
 
   // get gameAccountUUID
   let gameAccountUUID = result.id;
 
   // add the data to gw2Accounts collection
   await db
-  .collection(CollectionTypes.GAME_ACCOUNTS)
-  .doc(gameAccountUUID)
-  .set({
-    gameAccountUUID,
-    apiToken,
-    lastValid: new Date().toISOString(),
-    isFreeToPlay,
-    id: result.name
-  })
-  .catch(err => console.log(err));
+    .collection(CollectionTypes.GAME_ACCOUNTS)
+    .doc(gameAccountUUID)
+    .set({
+      gameAccountUUID,
+      apiToken,
+      lastValid: new Date().toISOString(),
+      isFreeToPlay,
+      id: result.name
+    })
+    .catch(err => console.log(err));
 
   // return that is is a success
   return { success: "API key added" };
@@ -76,4 +91,4 @@ async function updateF2P (apiToken){
 
 module.exports = {
   fixF2PStatus
-}
+};
