2dfbb0ce38aa0e3c775db0c319faab3b747a0536
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2020-11-24 09:52:02 +0100

    Including event name in Stage model

--- models/Stage.js
+++ models/Stage.js
@@ -4,20 +4,22 @@ class Stage {
   /**
    * @param {StageTypes} type - In which stage the event currently is
    * @param {string} year - The year of the event
+   * @param {string} name - The name of the event
    * @param {date} start - The start time for this stage
    * @param {date} end - The end time for this stage
    * @returns {Stage}
    */
-  constructor(type, year, start, end) {
+  constructor(type, year, name, start, end) {
     this.type = type;
     this.year = year;
+    this.name = name,
     this.start = typeof start === "object" ? start.toISOString() : start;
     this.end = typeof end === "object" ? end.toISOString() : end;
   }
 
   static fromEventData(data) {
-    const { isMatchingDone, year } = data;
-    let currentStage = new Stage(StageTypes.INACTIVE, year);
+    const { isMatchingDone, name, year } = data;
+    let currentStage = new Stage(StageTypes.INACTIVE, year, name);
     const eventEnd = data.eventEnd.toDate();
     const eventStart = data.eventStart.toDate();
     const signupStart = data.signupStart.toDate();
@@ -27,13 +29,14 @@ class Stage {
       currentStage = new Stage(
         StageTypes.SIGNUP,
         year,
+        name,
         signupStart,
         eventStart
       );
     } else if (eventStart < now && now < eventEnd && !isMatchingDone) {
-      currentStage = new Stage(StageTypes.MATCHING, year);
+      currentStage = new Stage(StageTypes.MATCHING, year, name);
     } else if (eventStart < now && now < eventEnd && isMatchingDone) {
-      currentStage = new Stage(StageTypes.GIFTING, year, eventStart, eventEnd);
+      currentStage = new Stage(StageTypes.GIFTING, year, name, eventStart, eventEnd);
     }
 
     return currentStage;
